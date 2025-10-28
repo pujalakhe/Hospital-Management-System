11425d90dbb60b779e6f1789f09979c418119e40
shrijana thapa magar <67953731+shrijana-thapa@users.noreply.github.com>
2025-10-28 15:56:24 -0700

    refactoring,using constant for snackbar type and action

--- src/app/shared/constants/snackbar-constants/snackbar.constants.ts
+++ src/app/shared/constants/snackbar-constants/snackbar.constants.ts
@@ -3,3 +3,19 @@ export const SNACKBAR_DURATION = {
   MEDIUM: 4000,
   LONG: 5000,
 };
+export const SNACKBAR_POSITION = {
+  HORIZONTAL: 'right' as const,
+  VERTICAL: 'top' as const,
+};
+export const SNACKBAR_TYPE = {
+  SUCCESS: 'success' as const,
+  ERROR: 'error' as const,
+  INFO: 'info' as const,
+  WARNING: 'warning' as const,
+};
+export const SNACKBAR_ACTION = {
+  CLOSE: 'Close' as const,
+  UNDO: 'Undo' as const,
+};
+export type SnackbarType = 'success' | 'error' | 'info' | 'warning';
+export type SnackbarAction = 'Close' | 'Undo';
