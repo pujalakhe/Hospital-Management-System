2c88b1b87bbbf5c280cf129765d7e92614ef6a90
puja lakhe <lakhepuza924@gmail.com>
2025-10-28 10:38:29 +0545

    refactor making the constant value readonly

--- src/app/shared/constants/dialog.constant.ts
+++ src/app/shared/constants/dialog.constant.ts
@@ -6,11 +6,11 @@ export const DIALOG_ACTIONS = {
 
 export const DIALOG_WIDTH = {
   width: '350px',
-};
+} as const;
 
 export const DIALOG_ACTIONS_COLOR = {
   WARN: 'warn',
   PRIMARY: 'primary',
-};
+} as const;
 
 export type DialogAction = (typeof DIALOG_ACTIONS)[keyof typeof DIALOG_ACTIONS];
