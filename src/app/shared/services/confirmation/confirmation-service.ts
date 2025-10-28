37561313f5e45ba41bd5ec3ebea2dc036c1df3d5
puja lakhe <lakhepuza924@gmail.com>
2025-10-28 10:24:49 +0545

    confirmation service

---
+++ src/app/shared/services/confirmation/confirmation-service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import { ConfirmationDialogComponent } from '../../components/confirmation-dialog-component/confirmation-dialog-component';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogData } from '../../model/confirm-dialog-data';
+import { DIALOG_WIDTH } from '../../constants/dialog.constant';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfirmationService {
+  constructor(private dialog: MatDialog) {}
+
+  confirm(data: ConfirmDialogData) {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: DIALOG_WIDTH.width,
+      data,
+    });
+    return dialogRef.afterClosed();
+  }
+}
