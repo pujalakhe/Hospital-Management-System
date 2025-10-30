6ece858966057b4d5c01ea89572d072bb7efe884
puja lakhe <lakhepuza924@gmail.com>
2025-10-30 10:23:25 +0545

    feat: implement authentication routing and constants

--- src/app/features/public/auth-module/auth-module.ts
+++ src/app/features/public/auth-module/auth-module.ts
@@ -2,18 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing-module';
-import { Logincomponent } from './components/logincomponent/logincomponent';
 import { LoginComponent } from './components/login-component/login-component';
 
-
 @NgModule({
-  declarations: [
-    Logincomponent,
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [LoginComponent],
+  imports: [CommonModule, AuthRoutingModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/features/public/auth-module/auth-routing-module.ts
+++ src/app/features/public/auth-module/auth-routing-module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login-component/login-component';
+import { ROUTER_PATHS } from '../../../core/constants/router-path.constant';
 
-const routes: Routes = [];
+const { LOGIN } = ROUTER_PATHS;
+const routes: Routes = [
+  { path: '', redirectTo: LOGIN, pathMatch: 'full' },
+  {
+    path: LOGIN,
+    component: LoginComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
