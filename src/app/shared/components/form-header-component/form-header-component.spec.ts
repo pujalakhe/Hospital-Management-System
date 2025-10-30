71aa541923c89b20ac10f90dc12776bda9793c43
puja lakhe <lakhepuza924@gmail.com>
2025-10-30 11:34:58 +0545

    unit testing done

--- src/app/shared/components/form-header-component/form-header-component.spec.ts
+++ src/app/shared/components/form-header-component/form-header-component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormHeaderComponent } from './form-header-component';
+import { By } from '@angular/platform-browser';
 
 describe('FormHeaderComponent', () => {
   let component: FormHeaderComponent;
@@ -8,9 +9,8 @@ describe('FormHeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FormHeaderComponent]
-    })
-    .compileComponents();
+      declarations: [FormHeaderComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FormHeaderComponent);
     component = fixture.componentInstance;
@@ -19,5 +19,20 @@ describe('FormHeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should display the heading input', () => {
+    component.heading = 'Main Heading';
+    fixture.detectChanges();
+
+    const headingEl = fixture.debugElement.query(By.css('h1'));
+    expect(headingEl.nativeElement.textContent).toContain('Main Heading');
+  });
+
+  it('should display the subHeading input', () => {
+    component.subHeading = 'Sub Heading';
+    fixture.detectChanges();
+
+    const subHeadingEl = fixture.debugElement.query(By.css('p'));
+    expect(subHeadingEl.nativeElement.textContent).toContain('Sub Heading');
   });
 });
