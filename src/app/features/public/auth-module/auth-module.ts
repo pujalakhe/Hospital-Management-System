cdf2f365222bf08baf296b5256c176ed9d6c7100
puja lakhe <lakhepuza924@gmail.com>
2025-10-30 11:33:28 +0545

    import necessary module to auth module

--- src/app/features/public/auth-module/auth-module.ts
+++ src/app/features/public/auth-module/auth-module.ts
@@ -3,9 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing-module';
 import { LoginComponent } from './components/login-component/login-component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TextInputComponent } from '../../../shared/components/inputs/text-input/text-input.component';
+import { FormHeaderComponent } from '../../../shared/components/form-header-component/form-header-component';
+import { MaterialModule } from '../../../shared/angular-material.module';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    TextInputComponent,
+    FormHeaderComponent,
+    MaterialModule,
+  ],
 })
 export class AuthModule {}
