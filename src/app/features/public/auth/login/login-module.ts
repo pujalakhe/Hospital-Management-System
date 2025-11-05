4c6277f0c82514ad037ef42ac600b914a5fe0a1f
puja lakhe <lakhepuza924@gmail.com>
2025-11-05 13:24:49 +0545

    add storeModule and effectModule feature key

--- src/app/features/public/auth/login/login-module.ts
+++ src/app/features/public/auth/login/login-module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../../../../shared/angular-material.module';
 
 import { LoginRoutingModule } from './login-routing-module';
 import { LoginComponent } from './components/login-component/login-component';
-import { MaterialModule } from '../../../../shared/angular-material.module';
-import { ReactiveFormsModule } from '@angular/forms';
 import { FormHeaderComponent } from '../../../../shared/components/form-header-component/form-header-component';
 import { LoaderComponent } from '../../../../shared/components/loader-component/loader-component';
 import { TextInputComponent } from '../../../../shared/components/inputs/text-input/text-input.component';
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+import { LOGIN_FEATURE_SELECTOR_KEY } from './store/login.constant';
+
+import { LoginEffects } from './store/login.effect';
+import { loginReducer } from './store/login.reducer';
 
 @NgModule({
   declarations: [LoginComponent],
@@ -19,6 +27,8 @@ import { TextInputComponent } from '../../../../shared/components/inputs/text-in
     FormHeaderComponent,
     LoaderComponent,
     TextInputComponent,
+    StoreModule.forFeature(LOGIN_FEATURE_SELECTOR_KEY, loginReducer),
+    EffectsModule.forFeature(LoginEffects),
   ],
 })
 export class LoginModule {}
