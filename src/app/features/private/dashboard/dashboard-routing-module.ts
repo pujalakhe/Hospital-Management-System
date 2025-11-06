9892ec4e89e03bf29f02942547f91873be9d0763
puja lakhe <lakhepuza924@gmail.com>
2025-11-06 13:14:45 +0545

    use of route constants

--- src/app/features/private/dashboard/dashboard-routing-module.ts
+++ src/app/features/private/dashboard/dashboard-routing-module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
+import { ROUTER_PATHS } from '../../../core/constants/router-path.constant';
 
 const routes: Routes = [
   {
-    path: 'dashboard',
+    path: ROUTER_PATHS.DASHBOARD,
     component: Dashboard,
   },
 ];
