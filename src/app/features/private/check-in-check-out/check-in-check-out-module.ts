241367ffd19c5cd545db0b0258c09acef5413977
puja lakhe <lakhepuza924@gmail.com>
2025-11-07 16:10:45 +0545

    no need for routing in this module

--- src/app/features/private/check-in-check-out/check-in-check-out-module.ts
+++ src/app/features/private/check-in-check-out/check-in-check-out-module.ts
@@ -1,20 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { CheckInCheckOutRoutingModule } from './check-in-check-out-routing-module';
-import { MaterialModule } from '../../../shared/angular-material.module';
-import { CheckInCheckOutDialogBoxComponent } from './components/check-in-check-out-dialog-box-component/check-in-check-out-dialog-box-component';
-import { TextInputComponent } from '../../../shared/components/inputs/text-input/text-input.component';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../../../shared/angular-material.module';
+import { SelectInputComponent } from '../../../shared/components/inputs/select-input/select-input.component';
+import { TextInputComponent } from '../../../shared/components/inputs/text-input/text-input.component';
 import { AutoFocusInput } from '../../../shared/custom-directives/AutoFocusInput/auto-focus-input';
 import { AutoFocusInvalid } from '../../../shared/custom-directives/AutoFocusInvalid/auto-focus-invalid';
-import { SelectInputComponent } from '../../../shared/components/inputs/select-input/select-input.component';
+import { CheckInCheckOutDialogBoxComponent } from './components/check-in-check-out-dialog-box-component/check-in-check-out-dialog-box-component';
 
 @NgModule({
   declarations: [CheckInCheckOutDialogBoxComponent],
   imports: [
     CommonModule,
-    CheckInCheckOutRoutingModule,
     MaterialModule,
     ReactiveFormsModule,
     TextInputComponent,
