d7fd220ec9881c1228b72c1e85c1b6fe62cb40be
shrijana thapa magar <67953731+shrijana-thapa@users.noreply.github.com>
2025-11-07 10:47:46 -0800

    used role pipe

--- src/app/features/public/auth/signup/signup-module.ts
+++ src/app/features/public/auth/signup/signup-module.ts
@@ -1,36 +1,35 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { SignupRoutingModule } from './signup-routing-module';
-import { SignupComponent } from './component/signup-component';
-import { MaterialModule } from '../../../../shared/angular-material.module';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { TextInputComponent } from '../../../../shared/components/inputs/text-input/text-input.component';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { MaterialModule } from '../../../../shared/angular-material.module';
+import { DateInputComponent } from '../../../../shared/components/inputs/date-input/date-input.component';
 import { InputNumberComponent } from '../../../../shared/components/inputs/input-number-component/input-number-component';
 import { SelectInputComponent } from '../../../../shared/components/inputs/select-input/select-input.component';
-import { DateInputComponent } from '../../../../shared/components/inputs/date-input/date-input.component';
-import { StoreModule } from '@ngrx/store';
-import { signupReducer } from './store/signup.reducer';
-import { EffectsModule } from '@ngrx/effects';
-import { signupEffects } from './store/signup.effects';
-import { SIGNUP_FEATURE_SELECTOR_KEY } from './constants/signup.constant';
-import { cityReducer } from '../../../../shared/store/city-list-by-countryId/cityListByCountryId.reducer';
+import { TextInputComponent } from '../../../../shared/components/inputs/text-input/text-input.component';
 import { CITY_FEATURE_SELECTOR_KEY } from '../../../../shared/store/city-list-by-countryId/cityListByCountryId.constant';
 import { CityByCountryIdEffects } from '../../../../shared/store/city-list-by-countryId/cityListByCountryId.effect';
+import { cityReducer } from '../../../../shared/store/city-list-by-countryId/cityListByCountryId.reducer';
 import { COUNTRY_FEATURE_SELECTOR_KEY } from '../../../../shared/store/country-list/countryList.constant';
-import { countryReducer } from '../../../../shared/store/country-list/countryList.reducer';
 import { CountryEffects } from '../../../../shared/store/country-list/countryList.effect';
+import { countryReducer } from '../../../../shared/store/country-list/countryList.reducer';
 import { DEPARTMENT_FEATURE_SELECTOR_KEY } from '../../../../shared/store/department-list/departmentList.constant';
-import { departmentReducer } from '../../../../shared/store/department-list/departmentList.reducer';
 import { DepartmentEffects } from '../../../../shared/store/department-list/departmentList.effect';
+import { departmentReducer } from '../../../../shared/store/department-list/departmentList.reducer';
 import { ROLE_FEATURE_SELECTOR_KEY } from '../../../../shared/store/role-list/roleList.constant';
-import { roleReducer } from '../../../../shared/store/role-list/roleList.reducer';
 import { RoleEffects } from '../../../../shared/store/role-list/roleList.effect';
-// import { RadioInputComponent } from '../../../../shared/components/inputs/radio-input/radio-input.component';
-import { GENDER_FEATURE_SELECTOR_KEY } from '../../../../shared/store/gender-list/genderList.constant';
-import { genderReducer } from '../../../../shared/store/gender-list/genderList.reducer';
-import { GenderEffects } from '../../../../shared/store/gender-list/genderList.effect';
+import { roleReducer } from '../../../../shared/store/role-list/roleList.reducer';
+import { SignupComponent } from './component/signup-component';
+import { SIGNUP_FEATURE_SELECTOR_KEY } from './constants/signup.constant';
+import { SignupRoutingModule } from './signup-routing-module';
+import { signupEffects } from './store/signup.effects';
+import { signupReducer } from './store/signup.reducer';
 import { FormHeaderComponent } from '../../../../shared/components/form-header-component/form-header-component';
+import { TransformRolePipe } from '../../../../shared/pipes/transform-object/transform-object-pipe';
+import { GENDER_FEATURE_SELECTOR_KEY } from '../../../../shared/store/gender-list/genderList.constant';
+import { GenderEffects } from '../../../../shared/store/gender-list/genderList.effect';
+import { genderReducer } from '../../../../shared/store/gender-list/genderList.reducer';
 
 @NgModule({
   declarations: [SignupComponent],
@@ -44,6 +43,7 @@ import { FormHeaderComponent } from '../../../../shared/components/form-header-c
     SelectInputComponent,
     DateInputComponent,
     FormHeaderComponent,
+    TransformRolePipe,
     StoreModule.forFeature(SIGNUP_FEATURE_SELECTOR_KEY, signupReducer),
     StoreModule.forFeature(CITY_FEATURE_SELECTOR_KEY, cityReducer),
     StoreModule.forFeature(COUNTRY_FEATURE_SELECTOR_KEY, countryReducer),
