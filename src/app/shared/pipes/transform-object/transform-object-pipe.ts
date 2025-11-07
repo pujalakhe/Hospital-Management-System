b68acf2ce7e5e7cb5cafdc87a0335c8cac9dc0f8
shrijana thapa magar <67953731+shrijana-thapa@users.noreply.github.com>
2025-11-07 10:51:01 -0800

    remove console

--- src/app/shared/pipes/transform-object/transform-object-pipe.ts
+++ src/app/shared/pipes/transform-object/transform-object-pipe.ts
@@ -6,10 +6,6 @@ import { Role } from '../../model/role.model';
 })
 export class TransformRolePipe implements PipeTransform {
   transform(response: Role[]): { id: number; name: string }[] {
-    console.log(response);
-    // if (!response || !response.data || !Array.isArray(response.data)) return [];
-    console.log('response');
-
     return response.map((item: Role) => ({
       id: item.key,
       name: item.value,
