e41dfdcea2af19ae240339c70998bf03e0e91e92
puja lakhe <lakhepuza924@gmail.com>
2025-11-10 14:18:40 +0545

    as per the status button label is being changed

--- src/app/features/private/dashboard/components/dashboard-component/dashboard-component.ts
+++ src/app/features/private/dashboard/components/dashboard-component/dashboard-component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../../../core/service/auth-service/auth-service';
 import { MatDialog } from '@angular/material/dialog';
 import { DIALOG_WIDTH } from '../../../../../shared/constants/dialog.constants';
 import { CheckInCheckOutDialogBoxComponent } from '../../../check-in-check-out/components/check-in-check-out-dialog-box-component/check-in-check-out-dialog-box-component';
+import { selectCheckedInStatus } from '../../../check-in-check-out/store/check-in-check-out.selectors';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-component',
@@ -10,8 +13,13 @@ import { CheckInCheckOutDialogBoxComponent } from '../../../check-in-check-out/c
   templateUrl: './dashboard-component.html',
   styleUrl: './dashboard-component.scss',
 })
-export class DashboardComponent {
-  constructor(private dialog: MatDialog) {}
+export class DashboardComponent implements OnInit {
+  checkInStatus$?: Observable<boolean>;
+  constructor(private dialog: MatDialog, private store: Store) {}
+  ngOnInit() {
+    this.checkInStatus$ = this.store.select(selectCheckedInStatus);
+  }
+
   openCheckInDialogBox() {
     this.dialog.open(CheckInCheckOutDialogBoxComponent, {
       width: DIALOG_WIDTH,
