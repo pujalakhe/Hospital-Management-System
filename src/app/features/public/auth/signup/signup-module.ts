f6f89129ac56de92bc0392018b6eaf1e3c65cb83
shrijana thapa magar <67953731+shrijana-thapa@users.noreply.github.com>
2025-11-10 14:12:45 -0800

    used directives in form auto focus first invalid and first input,cant paste in password

--- src/app/features/public/auth/signup/signup-module.ts
+++ src/app/features/public/auth/signup/signup-module.ts
@@ -21,7 +21,7 @@ import { ROLE_FEATURE_SELECTOR_KEY } from '../../../../shared/store/role-list/ro
 import { RoleEffects } from '../../../../shared/store/role-list/roleList.effect';
 import { roleReducer } from '../../../../shared/store/role-list/roleList.reducer';
 import { SignupComponent } from './component/signup-component';
-import { SIGNUP_FEATURE_SELECTOR_KEY } from './constants/signup.constant';
+import { SIGNUP_FEATURE_SELECTOR_KEY } from '../../auth/signup/store/signup.constants';
 import { SignupRoutingModule } from './signup-routing-module';
 import { signupEffects } from './store/signup.effects';
 import { signupReducer } from './store/signup.reducer';
@@ -31,6 +31,9 @@ import { GENDER_FEATURE_SELECTOR_KEY } from '../../../../shared/store/gender-lis
 import { GenderEffects } from '../../../../shared/store/gender-list/genderList.effect';
 import { genderReducer } from '../../../../shared/store/gender-list/genderList.reducer';
 import { LoaderComponent } from '../../../../shared/components/loader-component/loader-component';
+import { AutoFocusInput } from '../../../../shared/custom-directives/AutoFocusInput/auto-focus-input';
+import { AutoFocusInvalid } from '../../../../shared/custom-directives/AutoFocusInvalid/auto-focus-invalid';
+import { NoPasteDirective } from '../../../../shared/custom-directives/no-paste/no-paste-directive';
 
 @NgModule({
   declarations: [SignupComponent],
@@ -46,6 +49,9 @@ import { LoaderComponent } from '../../../../shared/components/loader-component/
     FormHeaderComponent,
     TransformRolePipe,
     LoaderComponent,
+    AutoFocusInput,
+    AutoFocusInvalid,
+    NoPasteDirective,
     StoreModule.forFeature(SIGNUP_FEATURE_SELECTOR_KEY, signupReducer),
     StoreModule.forFeature(CITY_FEATURE_SELECTOR_KEY, cityReducer),
     StoreModule.forFeature(COUNTRY_FEATURE_SELECTOR_KEY, countryReducer),
