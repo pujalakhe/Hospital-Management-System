64b836edce2bba18acc40fdf35313a8f817b079c
shrijana thapa magar <67953731+shrijana-thapa@users.noreply.github.com>
2025-11-10 14:38:46 -0800

    use environment for register epmloyee

--- src/app/features/public/auth/signup/service/api/signup-api-service.ts
+++ src/app/features/public/auth/signup/service/api/signup-api-service.ts
@@ -2,13 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SignupRequest, SignupResponse } from '../../models/signup.model';
+import { baseUserApi } from '../../../../../../../environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SignupApiService {
-  private ApiUrl_Signup =
-    'https://zb9qd6n8-14650.inc1.devtunnels.ms/api/Employee/RegisterEmployee';
+  private ApiUrl_Signup = `${baseUserApi}/Employee/RegisterEmployee`;
   constructor(private http: HttpClient) {}
   signup(payload: SignupRequest): Observable<SignupResponse> {
     return this.http.post<SignupResponse>(this.ApiUrl_Signup, payload);
