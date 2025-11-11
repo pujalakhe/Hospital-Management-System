43e2132dd8eba80a0e8a30dbade229793795ca35
puja lakhe <lakhepuza924@gmail.com>
2025-11-11 10:35:40 +0545

    static dashboard UI

--- src/app/features/private/dashboard/components/dashboard-component/dashboard-component.ts
+++ src/app/features/private/dashboard/components/dashboard-component/dashboard-component.ts
@@ -16,6 +16,30 @@ import { Observable } from 'rxjs';
 export class DashboardComponent implements OnInit {
   checkInStatus$?: Observable<boolean>;
   constructor(private dialog: MatDialog, private store: Store) {}
+
+  todoList: { title: string; description: string }[] = [
+    {
+      title: 'Complete onboarding document upload',
+      description: ' This is a summary of the content',
+    },
+    {
+      title: 'Follow up on clients',
+      description: ' This is a summary of the content',
+    },
+    {
+      title: 'Design wireframe for LMS',
+      description: ' This is a summary of the content',
+    },
+  ];
+
+  displayedColumns: string[] = ['earning', 'amount', 'deduction', 'total'];
+
+  paySlip = [
+    { earning: 'Basic Wage', amount: 150000, deduction: -30000, total: 120000 },
+    { earning: 'Tax', amount: 15000, deduction: -3000, total: 12000 },
+    { earning: 'Pension', amount: 15000, deduction: -3000, total: 12000 },
+  ];
+
   ngOnInit() {
     this.checkInStatus$ = this.store.select(selectCheckedInStatus);
   }
