1ee45ead98afda97c7a964524f3ffb27d863f279
puja lakhe <lakhepuza924@gmail.com>
2025-11-11 10:36:07 +0545

    refactor: enhance check-in/check-out dialog box component with improved form validation and state management

--- src/app/features/private/check-in-check-out/components/check-in-check-out-dialog-box-component/check-in-check-out-dialog-box-component.ts
+++ src/app/features/private/check-in-check-out/components/check-in-check-out-dialog-box-component/check-in-check-out-dialog-box-component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { checkInRequest } from '../../store/check-in-check-out.actions';
-import { Observable } from 'rxjs';
+import {
+  checkInRequest,
+  checkOutRequest,
+} from '../../store/check-in-check-out.actions';
+import { firstValueFrom, Observable } from 'rxjs';
 import { CheckInCheckOutFormService } from '../../service/check-in-check-out-form-service/check-in-check-out-form-service';
+import {
+  selectCheckedInStatus,
+  selectError,
+  selectLoading,
+} from '../../store/check-in-check-out.selectors';
 
 @Component({
   selector: 'app-check-in-check-out-dialog-box-component',
@@ -13,30 +21,68 @@ import { CheckInCheckOutFormService } from '../../service/check-in-check-out-for
   styleUrl: './check-in-check-out-dialog-box-component.scss',
 })
 export class CheckInCheckOutDialogBoxComponent implements OnInit {
-  checkInForm?: FormGroup;
+  checkInCheckOutForm?: FormGroup;
   loading$?: Observable<boolean>;
+  checkInStatus$?: Observable<boolean>;
+  error$?: Observable<string | null>;
   workLocationOptions: { id: number; name: string }[] = [
     { id: 1, name: 'Work from office' },
     { id: 2, name: 'Work from home' },
   ];
+
   constructor(
     private dialogRef: MatDialogRef<CheckInCheckOutDialogBoxComponent>,
     private checkInFormService: CheckInCheckOutFormService,
     private store: Store
   ) {}
+
   ngOnInit() {
     this.buildForm();
+    this.initializeSelectors();
   }
   buildForm() {
-    this.checkInForm = this.checkInFormService.buildCheckInCheckOutForm();
+    this.checkInCheckOutForm =
+      this.checkInFormService.buildCheckInCheckOutForm();
+  }
+
+  initializeSelectors() {
+    this.loading$ = this.store.select(selectLoading);
+    this.error$ = this.store.select(selectError);
+    this.checkInStatus$ = this.store.select(selectCheckedInStatus);
+    this.store
+      .select(selectCheckedInStatus)
+      .subscribe((res) => console.log(res));
+    this.renderFormAsPerTheStatus();
+  }
+
+  renderFormAsPerTheStatus() {
+    this.checkInStatus$?.subscribe((status) => {
+      if (!status) {
+        // Check In
+        this.getControl('checkInReason').setValidators([Validators.required]);
+        this.getControl('workLocation').setValidators([Validators.required]);
+        this.getControl('checkOutReason').clearValidators();
+      } else {
+        // Check Out
+        this.getControl('checkOutReason').setValidators([Validators.required]);
+        this.getControl('checkInReason').clearValidators();
+        this.getControl('workLocation').clearValidators();
+      }
+
+      // Update validity after changing validators
+      this.checkInCheckOutForm?.updateValueAndValidity();
+    });
   }
 
   getControl(controlName: string): FormControl {
-    return this.checkInForm?.get(controlName) as FormControl;
+    return this.checkInCheckOutForm?.get(controlName) as FormControl;
   }
+
   onSubmit() {
-    if (this.checkInForm?.invalid) return;
-    const payload = this.checkInForm?.value;
+    this.checkInCheckOutForm?.updateValueAndValidity();
+    if (this.checkInCheckOutForm?.invalid) return;
+    const payload = this.checkInCheckOutForm?.value;
+    console.log(payload);
     this.store.dispatch(checkInRequest({ payload }));
     // this.dialogRef.close(true);
   }
