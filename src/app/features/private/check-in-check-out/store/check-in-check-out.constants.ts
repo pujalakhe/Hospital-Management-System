dc1d35647fb509adf725aebd47f9c80f95ec68fe
puja lakhe <lakhepuza924@gmail.com>
2025-11-12 10:56:25 +0545

    dd more unique action names

--- src/app/features/private/check-in-check-out/store/check-in-check-out.constants.ts
+++ src/app/features/private/check-in-check-out/store/check-in-check-out.constants.ts
@@ -1,16 +1,16 @@
 export const CHECK_IN_FEATURE_SELECTOR_KEY = 'Attendance';
 
-export const CHECK_IN_REQUEST = `[${CHECK_IN_FEATURE_SELECTOR_KEY}] Request`;
+export const CHECK_IN_REQUEST = `[${CHECK_IN_FEATURE_SELECTOR_KEY}]Check In Request`;
 
-export const CHECK_IN_SUCCESS = `[${CHECK_IN_FEATURE_SELECTOR_KEY}] Success`;
+export const CHECK_IN_SUCCESS = `[${CHECK_IN_FEATURE_SELECTOR_KEY}]Check In Success`;
 
-export const CHECK_IN_FAILURE = `[${CHECK_IN_FEATURE_SELECTOR_KEY}] Failure`;
+export const CHECK_IN_FAILURE = `[${CHECK_IN_FEATURE_SELECTOR_KEY}]Check In Failure`;
 
-export const CHECK_OUT_REQUEST = `[${CHECK_IN_FEATURE_SELECTOR_KEY}] Request`;
+export const CHECK_OUT_REQUEST = `[${CHECK_IN_FEATURE_SELECTOR_KEY}] Check Out Request`;
 
-export const CHECK_OUT_SUCCESS = `[${CHECK_IN_FEATURE_SELECTOR_KEY}] Success`;
+export const CHECK_OUT_SUCCESS = `[${CHECK_IN_FEATURE_SELECTOR_KEY}]Check Out Success`;
 
-export const CHECK_OUT_FAILURE = `[${CHECK_IN_FEATURE_SELECTOR_KEY}] Failure`;
+export const CHECK_OUT_FAILURE = `[${CHECK_IN_FEATURE_SELECTOR_KEY}]  Check Out Failure`;
 
 export const CHECK_IN_STATUS = `[${CHECK_IN_FEATURE_SELECTOR_KEY}] Load CheckIn Status`;
 
