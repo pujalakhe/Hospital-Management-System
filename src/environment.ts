476e046b813a70f64b694a363c9e38874959da17
puja lakhe <lakhepuza924@gmail.com>
2025-11-14 10:22:38 +0545

    base api changed

--- src/environment.ts
+++ src/environment.ts
@@ -1,2 +1,2 @@
 export const baseUserApi = 'https://zb9qd6n8-14650.inc1.devtunnels.ms/api';
-export const baseAttendanceApi = 'https://vjb9dgzs-2209.inc1.devtunnels.ms/api';
+export const baseAttendanceApi = 'https://1238r13n-2209.inc1.devtunnels.ms/api';
