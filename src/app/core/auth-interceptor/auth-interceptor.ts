f2562fdbfd735bff034fac63cd7102f8b6d59763
puja lakhe <lakhepuza924@gmail.com>
2025-11-14 10:05:38 +0545

    when unauthorized redirect user to login

--- src/app/core/auth-interceptor/auth-interceptor.ts
+++ src/app/core/auth-interceptor/auth-interceptor.ts
@@ -1,13 +1,22 @@
-import { HttpRequest, HttpResponse } from '@angular/common/http';
+import {
+  HttpErrorResponse,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
 import { HttpInterceptorFn, HttpHandlerFn } from '@angular/common/http';
-import { tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { AUTH_TOKEN_KEY } from '../constants/storage.constants';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
+import { ROUTER_PATHS } from '../constants/router-path.constant';
 
 export const authInterceptor: HttpInterceptorFn = (
   req,
   next: HttpHandlerFn
 ) => {
   const token = localStorage.getItem(AUTH_TOKEN_KEY);
+  const router = inject(Router); // Angular v18+
 
   const modifiedReq = token
     ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
@@ -20,6 +29,14 @@ export const authInterceptor: HttpInterceptorFn = (
       if (event instanceof HttpResponse) {
         // console.log('[Response received]', event.status, event.url);
       }
+    }),
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401 || error.status === 403) {
+        localStorage.removeItem(AUTH_TOKEN_KEY);
+        router.navigate([ROUTER_PATHS.LOGIN]);
+      }
+
+      return throwError(() => error);
     })
   );
 };
